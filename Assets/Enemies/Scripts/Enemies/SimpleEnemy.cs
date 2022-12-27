4ddee5abad636078e57a09393ccaa14e1d72d8c8
Creator-Lynx <1_linx_1@mail.ru>
2022-12-27 16:40:03 +0300

    modificated moving behaviour added extended first pattern of moving now can changed from inspector and coolest testing

--- Assets/Enemies/Scripts/Enemies/SimpleEnemy.cs
+++ Assets/Enemies/Scripts/Enemies/SimpleEnemy.cs
@@ -4,10 +4,14 @@ public class SimpleEnemy : Enemy
 {
     [SerializeField]
     GameObject bulletPrefab;
+    [SerializeField]
+    Transform[] wayPoints;
     void Start()
     {
         damagable = new SimpleDamagable(gameObject);
-        patrolable = new NavMeshPatrolBehavior(gameObject);
+        Vector3[] points = new Vector3[wayPoints.Length];
+        for (int i = 0; i < wayPoints.Length; i++) { points[i] = wayPoints[i].position; }
+        patrolable = new NavMeshPatrolBehavior(gameObject, points, 2f);
         searchable = new OldSearch(transform);
         shootable = new OldShoot(transform, bulletPrefab);
     }
